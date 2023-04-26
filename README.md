fb3d5ff24b6a5db5a4ff4c97cab75d27dd2a47e3
haron-lee <haronxlee@gmail.com>
2023-04-26 23:02:53 +0900

    Feat: 안내 알림창 추가

---
+++ README.md
@@ -0,0 +1,20 @@
+# 📝 My Memo
+
+## 👀 preview
+
+![mymemo](https://user-images.githubusercontent.com/88657261/234598664-6fd407e6-aeed-4111-86ff-53dd0911466d.png)
+
+## 🚀 목표와 기능
+
+- 제목과 콘텐츠 value를 받아오기
+- 받아온 값을 저장하기 버튼을 누르면 로컬스토리지에 저장되면서, HTML태그와 받아온 값을 그 태그안에 textContent로 넣어준다.
+- 생성된 수정과 삭제 버튼에 기능을 넣어준다.
+- 중앙의 생성되는 HTML태그들의 위치가 가득 찰 경우에는 scroll이 되게끔 구현
+- New note 버튼은 새로운 메모를 작성할 수 있게 해야하기에 input과 textarea의 내용이 있다면 지워준다.
+- 메모가 저장될 때마다 최근 메모가 위로갈 수 있도록 구현. (insertBefore사용)
+
+<br>
+
+## 🛠️ 개선해야할 점
+
+- 수정, 삭제 후에도 title을 눌렀을 때 디테일 창이 나타나야한다.
---
+++ index.js
@@ -0,0 +1,161 @@
+const saveBtn = document.getElementById('save_btn');
+const listTitle = document.getElementById('title');
+const listContent = document.getElementById('content');
+
+let allMemo = JSON.parse(localStorage.getItem('allMemo'));
+allMemo = allMemo ?? [];
+render();
+
+//* note를 localStorage에 저장
+function saveNote() {
+  const title = document.getElementById('title').value;
+  const content = document.getElementById('content').value;
+  const modifyIdx = saveBtn.getAttribute('data-idx');
+
+  // title과 content가 비어있다면 알림이 가게끔
+  if (title === '' || content === '') {
+    alert('제목 또는 내용을 작성해주세요');
+  }
+  // saveBtn이 "저장하기"여야하고, title과 content에 내용이 있어야 저장 실행
+  if (saveBtn.textContent === '저장하기' && !(!title || !content)) {
+    allMemo.push({ title, content, len: allMemo.length });
+    localStorage.setItem('allMemo', JSON.stringify(allMemo));
+    // 저장하기 누르면 내부 value값은 없어지게
+    listTitle.value = '';
+    listContent.value = '';
+  } else if (saveBtn.textContent === '수정하기' && modifyIdx !== null) {
+    allMemo[modifyIdx].title = title;
+    allMemo[modifyIdx].content = content;
+    localStorage.setItem('allMemo', JSON.stringify(allMemo));
+    listTitle.value = '';
+    listContent.value = '';
+    saveBtn.textContent = '저장하기';
+    saveBtn.removeAttribute('data-idx');
+  }
+  render();
+}
+
+function render() {
+  const noteDisplay = document.getElementById('note_display');
+  noteDisplay.innerHTML = '';
+
+  for (const item of allMemo) {
+    const saveBox = document.createElement('li');
+    const saveTitle = document.createElement('h2');
+    const saveContent = document.createElement('p');
+    const saveId = document.createElement('p');
+    const btnBox = document.createElement('div');
+    const modifyBtn = document.createElement('button');
+    const deleteMemoBtn = document.createElement('button');
+
+    saveBox.classList.add('list');
+    saveId.classList.add('list-id');
+    saveTitle.classList.add('list-title');
+    saveContent.classList.add('list-content');
+    btnBox.classList.add('box-btn');
+    modifyBtn.classList.add('list-modify');
+    deleteMemoBtn.classList.add('list-delete');
+
+    saveTitle.textContent = item.title;
+    saveContent.textContent = item.content;
+    saveId.textContent = item.len + 1;
+    modifyBtn.textContent = '수정';
+    modifyBtn.setAttribute('id', item.len);
+    modifyBtn.setAttribute('onclick', 'modify()');
+    deleteMemoBtn.textContent = '삭제';
+    deleteMemoBtn.setAttribute('id', item.len);
+    deleteMemoBtn.setAttribute('onclick', 'remove()');
+
+    saveBox.appendChild(saveId);
+    saveBox.appendChild(saveTitle);
+    saveBox.appendChild(saveContent);
+    btnBox.appendChild(modifyBtn);
+    btnBox.appendChild(deleteMemoBtn);
+    saveBox.appendChild(btnBox);
+    // noteDisplay.appendChild(saveBox);
+    // 최신목록이 위로 올라가게끔 하기
+    noteDisplay.insertBefore(saveBox, noteDisplay.firstChild);
+  }
+}
+
+const modify = () => {
+  const idx = allMemo.findIndex((item) => item.len == event.srcElement.id);
+  if (idx !== -1) {
+    listTitle.value = allMemo[idx].title;
+    listContent.value = allMemo[idx].content;
+    saveBtn.textContent = '수정하기';
+    // 수정 버튼의 id를 변경하여 수정할 항목을 식별하도록 함
+    saveBtn.setAttribute('data-idx', idx);
+  }
+};
+
+function remove() {
+  const idx = allMemo.find((item) => item.len == event.srcElement.id);
+  if (idx) {
+    allMemo.splice(
+      allMemo.findIndex((item) => item.len == idx.len),
+      1
+    );
+  }
+  // 삭제하고 한번더 세팅
+  localStorage.setItem('allMemo', JSON.stringify(allMemo));
+  render();
+}
+
+document.querySelector('#newNote').addEventListener('click', () => {
+  if (listTitle.value || listContent.value) {
+    listTitle.value = '';
+    listContent.value = '';
+  }
+});
+
+//* modal 구현하기
+const lists = document.querySelectorAll('.list .list-title');
+const listsContent = document.querySelectorAll('.list .list-content');
+
+const closeModal = () => {
+  const modal = document.querySelector('.modal');
+  modal.remove();
+};
+
+lists.forEach((list) => {
+  list.addEventListener('click', () => {
+    const modal = document.createElement('article');
+    const titleBox = document.createElement('div');
+    const modalTitle = document.createElement('h2');
+    const modalContent = document.createElement('p');
+    const closeBtn = document.createElement('button');
+
+    modal.classList.add('modal');
+    titleBox.classList.add('title-box');
+    modalTitle.classList.add('modal-title');
+    modalContent.classList.add('modal-content');
+    closeBtn.classList.add('close-btn');
+
+    closeBtn.setAttribute('onclick', 'closeModal()');
+
+    modal.appendChild(titleBox);
+    titleBox.appendChild(modalTitle);
+    titleBox.appendChild(closeBtn);
+    modal.appendChild(modalContent);
+    container.appendChild(modal);
+    // 클릭된 list-title의 textContent와, 형제의 textContent가져오기
+    modalTitle.textContent = list.textContent;
+    modalContent.textContent = list.nextElementSibling.textContent;
+  });
+});
+
+saveBtn.addEventListener('click', saveNote);
+
+//* Notification
+const notification = document.querySelector('#noti-container');
+const showNotification = () => {
+  notification.style.display = 'block';
+};
+
+const hideNotification = () => {
+  notification.style.display = 'none';
+};
+
+setTimeout(showNotification, 5000);
+setTimeout(hideNotification, 9000);
